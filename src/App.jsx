a1616b8f9eab736757df64c58c9f114f47a4fd0f
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-31 22:41:21 -0700

    Remove font and extra div, add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-merriweather text-stone-900">
+    <div className="font-merriweather flex min-h-screen flex-col items-center justify-center text-stone-900">
       <Hero />
+      <Footer />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="relative">
-      <div className="relative flex flex-col bg-stone-100">
-        <h1 className="p-16 text-center text-3xl font-bold">
-          Nick B. Schatzki
-          <br />
-          <span className="text-xl font-normal">Voice Artist</span>
-        </h1>
-        <img src="https://picsum.photos/200" alt="Image" />
-      </div>
+    <div className="flex flex-col rounded-xl bg-stone-100">
+      <h1 className="p-16 text-center text-2xl font-semibold">
+        Nick B. Schatzki
+        <hr className="mt-3 border border-stone-900" />
+        <span className="text-lg font-normal">Voice Artist</span>
+      </h1>
+      <img src="https://picsum.photos/200" alt="Hero Image" />
     </div>
   );
 }
